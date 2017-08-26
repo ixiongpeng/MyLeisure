eebb688d1fff6986a672903013da08d8152d4640
ixiongpeng <ixiongpeng@163.com>
2017-08-26 13:09:37 +0800

    change content for test,after reset add the text

--- app/src/main/java/app/ixiongpeng/com/myleisure/MainActivity.java
+++ app/src/main/java/app/ixiongpeng/com/myleisure/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//add contest for test,afterreset ,add the text
+        setContentView(R.layout.activity_main);//add contest for test,afterreset add the text
     }
 }
